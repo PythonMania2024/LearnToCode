45fae76a3033eed828eaf0d77ac50facab6013f0
PythonMania2024 <igjaguzkra@gmail.com>
2024-10-21 17:06:20 -0300

    Explicacion.py

---
+++ Python/Explicacion.py
@@ -0,0 +1,7 @@
+#Funciones y parametros
+def NombreDeLaFuncion(Parametros):#Los parametros  son peticiones Pueden ser definidos o normales.Definidos significa 
+  #que ya estan definidos como def Hola(x = 1)
+  print(Parametros)
+
+NombreDeLaFuncion(1)#Normal
+
